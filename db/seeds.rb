f755430a0bedd39fafbc3d73ff70eb3807111179
sergethompson <serge.thompson@gmail.com>
2014-01-15 10:28:51 -0500

    fixing forms

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,9 +18,9 @@ end
 
 
 10.times do
-	activity = %w(Run Skate Bike Ice-Skating Yoga Spin Concert Walk Dance Outdoor-Movie).sample
-	description = %w(Doing-it Watching-it Being-it Spectator).sample
-	address = ["Manhattan, New York", "Queens, New York", "Brooklyn, New York", "Flatiron District", "Greenwich, CT", "Bronx, New York", "Staten Island, NY", "San Jose, CA", "Nashville, TN", "Stowe, VT"].sample
+	activity = %w(Run Bike Swimming Racquetball).sample
+	description = "Will be a superfun time!"
+	address = ["Manhattan, New York", "Queens, New York", "Brooklyn, New York", "Flatiron District", "Bronx, New York", "Staten Island, NY"].sample
 	time = %w(6am 7am 8am 9am 4pm 5pm 6pm 7pm 8pm).sample
 	user = User.all.sample
 
